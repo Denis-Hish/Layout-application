92197d40fcc8fc24c88241fd947521682e8a4317
Denis-Hish <denis.hish@gmail.com>
2024-09-12 09:57:44 +0200

    Preliminary version

--- js/allertify.js
+++ js/allertify.js
@@ -33,3 +33,23 @@ export function Allertify() {
       alertify.notify('Warning', 'warning', timeShow);
     });
 }
+
+/* ------------------------------------------------------ */
+// const time = 5000;
+// document.querySelectorAll('.allerty').forEach(div => {
+//   const message = div.textContent.trim();
+//   const type = div.getAttribute('type-allerty');
+
+//   alertify.notify(message, type, time);
+// });
+
+function showAllerty(message, type) {
+  const time = 5000;
+  alertify.notify(message, type, time);
+}
+// showAllerty('Lorem ipsum dolor sit amet', 'message');
+// showAllerty('Error message', 'error');
+
+// document
+//   .querySelector('.test')
+//   .addEventListener('click', () => showAllerty('Error message', 'error'));
